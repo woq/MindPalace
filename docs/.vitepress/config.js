b98f76c359ef6cacffcc98572e4dde1fe880159b
woq <33190773+woq@users.noreply.github.com>
2021-10-15 17:43:25 +0800

    WIP 01
    add nav, front index

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,5 +2,12 @@ module.exports = {
     lang: 'zh-CN',
     title: 'Mind Palace',
     description: 'Finsh what we started.',
-    base: '/MindPalace/'
+    base: '/MindPalace/',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },                      // 根路径
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' }, // 外部链接
+      ],
+    }
   }
